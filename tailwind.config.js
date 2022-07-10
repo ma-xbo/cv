34bc6566f9e9b6a28a527ad890f46ff74e937df2
ma-xbo <max.bossert@outlook.de>
2022-07-10 11:21:06 +0200

    Initial version

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "*.html"],
+  darkMode: "media", // or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
